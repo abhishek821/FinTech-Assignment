3246c908909377065302a29ca4a2524a9cfcdea9
Abhishek Yadav <77781284+abhishek821@users.noreply.github.com>
2021-05-13 11:25:30 +0100

    Add files via upload

---
+++ myfirstfunction.js
@@ -0,0 +1,12 @@
+function changeImage(){
+var image = document.getElementById('myImage');
+
+if
+
+(image.src.match("bitcointrade"))
+
+{image.src="bitcoin.jpg";}
+else
+{image.src="bitcointrade.jpg";}
+
+}
